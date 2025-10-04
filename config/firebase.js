f9053954904771cc8fde20c7e98f28e167836465
HD21Virtual <159862918+HD21Virtual@users.noreply.github.com>
2025-10-04 20:46:53 -0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { initializeApp as initializeFirebaseApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 // Configuração do seu projeto Firebase
-// ATENÇÃO: As chaves de configuração devem ser mantidas em segredo em um ambiente de produção.
-// Aqui, elas são necessárias para o funcionamento no ambiente de demonstração.
 const firebaseConfig = {
     apiKey: "AIzaSyAbDQfS3VTVlXEBdHKKwx-ToTWTGFOcYAE",
     authDomain: "vade-mecum-de-questoes.firebaseapp.com",
@@ -14,9 +12,15 @@ const firebaseConfig = {
     appId: "1:667396734608:web:96f67c131ccbd798792215"
 };
 
-// Inicializa o Firebase
-const app = initializeApp(firebaseConfig);
+let app;
+export let auth;
+export let db;
 
-// Exporta as instâncias do Auth e Firestore para serem usadas em outros módulos
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export function initializeApp() {
+    if (!app) {
+        app = initializeFirebaseApp(firebaseConfig);
+        auth = getAuth(app);
+        db = getFirestore(app);
+    }
+}
+
