8b383bfb6a83fe3f2d07742bb65973e1420d167d
HD21Virtual <159862918+HD21Virtual@users.noreply.github.com>
2025-10-07 18:00:24 -0300

    Update question-viewer.js

--- js/features/question-viewer.js
+++ js/features/question-viewer.js
@@ -184,10 +184,7 @@ export function renderAnsweredQuestion(isCorrect, userAnswer, isFreshAnswer = fa
             `;
             footer.innerHTML = `<div class="w-full">${feedbackDiv.innerHTML} ${srsButtonsHTML}</div>`;
         } else {
-             const randomPercentage = (Math.random() * (85 - 60) + 60).toFixed(1);
-             feedbackDiv.innerHTML += `
-                <span class="text-sm text-gray-500">${randomPercentage}% acertaram</span>
-                <button class="view-resolution-btn text-sm text-blue-600 hover:underline">Ver resolução</button>
+                <button id="show-comment-btn" class="text-blue-600 hover:underline">Ver resolução</button>
              `;
              footer.appendChild(feedbackDiv);
         }
@@ -339,5 +336,6 @@ export function renderQuestionListForAdding(questions, existingQuestionIds) {
         `;
     }).join('');
 }
+
 
 
